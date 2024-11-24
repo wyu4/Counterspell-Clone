9021d90af3d97d19698d8dc56bedbd782c64a78d
Wilson <wilsonyu657@gmail.com>
2024-11-24 01:40:56 -0500

    Tutorial POC works!

--- src/Game/ScenePresets/Tutorial.java
+++ src/Game/ScenePresets/Tutorial.java
@@ -4,7 +4,8 @@ import Game.Scene;
 
 public class Tutorial extends Scene {
     private static final String G1 = "QUESTIONING1";
-    private static final String G2 = "QUESTIONING_DONE1";
+    private static final String G2 = "QUESTIONING2";
+    private static final String G3 = "QUESTIONING3";
     private String currentPlaying;
 
     public Tutorial() {
@@ -14,13 +15,24 @@ public class Tutorial extends Scene {
         super.addDialogue("???", "Welcome.");
         super.addDialogue("???", "Commencing cloning protocol…");
         super.addDialogue("???", "DATE: 21XX - 12 - 24");
-        super.addDialogue("???", "SUBJECT: MC NAME");
+        super.addDialogue("???", "SUBJECT: ANNE");
         super.addDialogue("???", "TEST #: 1056");
         super.addDialogue("???", "Loading...");
         super.addDialogue("???", "Complete.");
         super.addDialogue("???", "Please answer the following questions to the best of your ability:");
+        super.addDialogue("???", "Question 1: ___________________________");
         super.addDialogue("", "", G1);
-        super.addDialogue("???", "Loading…", G2);
+        super.addDialogue("???", "Question 2: ___________________________");
+        super.addDialogue("???", "", G2);
+        super.addDialogue("???", "Final question: ___________________________");
+        super.addDialogue("???", "", G3);
+        super.addDialogue("???", "Loading...");
+        super.addDialogue("???", "Data Successfully Collected!");
+        super.addDialogue("???", "Commencing Clone Generation Protocol…");
+        super.addDialogue("???", "Reducing clone statistics…");
+        super.addDialogue("???", "Generating body…");
+
+
         processInputs(true, false);
         requestNextDialogue();
         System.out.println("Scene1 created");
@@ -40,25 +52,52 @@ public class Tutorial extends Scene {
         currentPlaying = cue;
         switch (cue) {
             case G1: {
-                System.out.println("Switching!");
                 setDialoguePanelShowing(false);
                 setTypingPanelShowing(true);
-                setTypePrompt("Hello Test");
+                setTypePrompt("Hi");
 //                setTypePrompt("Adaptive radiation is the evolutionary process through which a single ancestral species diversifies into multiple distinct species, each adapted to exploit different ecological niches. This occurs rapidly, often in response to an environment with diverse resources or a lack of competition.");
                 processInputs(false, true);
                 setTypingPanelOnEmpty(
                         () -> {
                             System.out.println("Player completed first tutorial.");
+                            processInputs(true, false);
+                            setDialoguePanelShowing(true);
+                            setTypingPanelShowing(false);
                             requestNextDialogue();
                         }
                 );
                 break;
             }
             case G2: {
-                System.out.println("G2!");
-                processInputs(true, false);
-                setDialoguePanelShowing(true);
-                setTypingPanelShowing(false);
+                setDialoguePanelShowing(false);
+                setTypingPanelShowing(true);
+                setTypePrompt("CRISPR-Cas9 is a revolutionary gene-editing tool adapted from the immune system of prokaryotes. It enables precise modifications to DNA sequences.");
+                processInputs(false, true);
+                setTypingPanelOnEmpty(
+                        () -> {
+                            System.out.println("Player completed second tutorial.");
+                            processInputs(true, false);
+                            setDialoguePanelShowing(true);
+                            setTypingPanelShowing(false);
+                            requestNextDialogue();
+                        }
+                );
+                break;
+            }
+            case G3: {
+                setDialoguePanelShowing(false);
+                setTypingPanelShowing(true);
+                setTypePrompt("The human body maintains a stable internal temperature (~37C) through homeostasis, even in changing external conditions.");
+                processInputs(false, true);
+                setTypingPanelOnEmpty(
+                        () -> {
+                            System.out.println("Player completed third tutorial.");
+                            processInputs(true, false);
+                            setDialoguePanelShowing(true);
+                            setTypingPanelShowing(false);
+                            requestNextDialogue();
+                        }
+                );
                 break;
             }
         }
