afabfaf3fabb40887107e328b89efb6282fbdbee
Wilson <wilsonyu657@gmail.com>
2024-11-23 20:53:28 -0500

    Mistake count and WPM

--- src/Game/TypingPanel.java
+++ src/Game/TypingPanel.java
@@ -17,6 +17,9 @@ public class TypingPanel extends AccuratePanel {
     private String displayedText;
     private Font font;
     private float fontSize;
+    private int wordCount;
+    private int mistakes;
+    private long startTime;
 
     public TypingPanel() {
         this("The quick brown fox jumps over the lazy dog.");
@@ -29,6 +32,9 @@ public class TypingPanel extends AccuratePanel {
         displayedText = prompt;
         font = ResourcesManager.getAsFont(ResourceEnum.DroidSansMono_ttf);
         fontSize = 1;
+        startTime = System.currentTimeMillis();
+        wordCount = 0;
+        mistakes = 0;
         setBackground(new Color(45, 45, 45));
         setPrompt(message);
 
@@ -57,9 +63,22 @@ public class TypingPanel extends AccuratePanel {
             return;
         }
         if (displayedText.charAt(0) == key) {
+            if (displayedText.charAt(0) == ' ') {
+                wordCount ++;
+            }
             displayedText = displayedText.substring(1);
-            System.out.println("Got " + key);
+        } else {
+            mistakes ++;
+            textLabel.setForeground(new Color(255, 0, 0));
         }
+    }
+
+    public float calcWPM() {
+        return (wordCount/(float)(System.currentTimeMillis() - startTime))*60000f;
+    }
+
+    public int getMistakes() {
+        return mistakes;
     }
 
     public void tick(float timeMod) {
@@ -71,6 +90,14 @@ public class TypingPanel extends AccuratePanel {
             fontSize = newFontSize;
             textLabel.setFont(font.deriveFont(fontSize));
         }
+        Color oldFontColor = textLabel.getForeground();
+        textLabel.setForeground(
+                new Color(
+                        (int) Math.clamp(oldFontColor.getRed() + 5*timeMod,0,255),
+                        (int) Math.clamp(oldFontColor.getGreen() + 5*timeMod,0,255),
+                        (int) Math.clamp(oldFontColor.getBlue() + 5*timeMod,0,255)
+                )
+        );
         textLabel.setText(displayedText);
     }
 }
